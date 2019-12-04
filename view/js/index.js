8c8f657ab3a2622cfd3d764a3760e9bcb1e26752
Ibai acha <achita145@gmail.com>
2019-12-04 14:45:22 +0100

    Index.php

---
+++ view/js/index.js
@@ -0,0 +1,17 @@
+$('.carousel').carousel({
+    padding: 200    
+});
+autoplay();
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $('.dropdown-trigger').dropdown();
